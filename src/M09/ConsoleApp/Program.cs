89fe41f325fb992015b0fe8eda8cb514c2b02d0f
Asirin <asirin85@gmail.com>
2021-09-20 17:49:37 +0400

    Changes in main method

--- src/M09/ConsoleApp/Program.cs
+++ src/M09/ConsoleApp/Program.cs
@@ -15,10 +15,6 @@ namespace ConsoleApp
             Console.WriteLine("Write your arguments (-name, -minmark, -maxmark, -datefrom, -dateto, -test, -sort): ");
             string arguments = Console.ReadLine();
             List<Student> students = dataProc.GetData(arguments);
-            var query = from student in students
-                        where student.Name.Contains("")
-                        select student;
-            students = query.ToList();
             foreach(var student in students)
             {
                 Console.WriteLine($"{student.Name} {student.Test} {student.Date.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)} {student.Mark}");
